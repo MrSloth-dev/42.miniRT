8cce8550fa86573d6482307c3529bfbb37d3453f
isilva-t <ivanrfteixeira@gmail.com>
2024-11-26 15:20:18 +0000

    update norm_vector

--- src/01_oper/_0108_norm_vector.c
+++ src/01_oper/_0108_norm_vector.c
@@ -8,5 +8,6 @@ t_tuple	ft_norm_vector(t_tuple tuple)//p10
 	magn = ft_magn_tuple(tuple);
 	return ((t_tuple){tuple.x / magn,
 		tuple.y / magn,
-		tuple.z / magn, 1});
+		tuple.z / magn,
+		tuple.w / magn});
 }
