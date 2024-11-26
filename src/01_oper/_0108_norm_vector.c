d323353fea435cffa530a37dacad1469cd118490
isilva-t <ivanrfteixeira@gmail.com>
2024-11-26 15:13:13 +0000

    correct magn_tuple in pow 2 | adjust norm_vector to calculate magn_tuple only once

--- src/01_oper/_0108_norm_vector.c
+++ src/01_oper/_0108_norm_vector.c
@@ -2,8 +2,11 @@
 
 t_tuple	ft_norm_vector(t_tuple tuple)//p10
 {
+	double	magn;
+
 	ft_assert(tuple.w == 0, "Not a Vector");
-	return ((t_tuple){tuple.x / ft_magn_tuple(tuple),
-		tuple.y / ft_magn_tuple(tuple),
-		tuple.z / ft_magn_tuple(tuple), 1});
+	magn = ft_magn_tuple(tuple);
+	return ((t_tuple){tuple.x / magn,
+		tuple.y / magn,
+		tuple.z / magn, 1});
 }
