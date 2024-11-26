6c0e29c565264d24b39d2bdb2d5ce83322ac98f5
isilva-t <ivanrfteixeira@gmail.com>
2024-11-26 07:49:24 +0000

    update

--- src/01_oper/00_oper.c
+++ src/01_oper/00_oper.c
@@ -23,20 +23,3 @@ t_tuple	ft_mul_tuple(t_tuple tuple1, t_tuple tuple2)
 	result.z = tuple1.z * tuple2.z;
 	return (result);
 }
-
-int	ft_color_rgb_to_int(t_color color)
-{
-	return ((int)(color.r * 255.0f) << 16
-	| (int)(color.g * 255.0f) << 8
-	| (int)(color.g * 255.0f));
-}
-
-t_color	ft_color_int_to_rgb(int color)
-{
-	t_color	col;
-
-	col.r = (float)(color >> 16) / 255.0f;
-	col.r = (float)(color >> 8) / 255.0f;
-	col.r = (float)color / 255.0f;
-	return (col);
-}
