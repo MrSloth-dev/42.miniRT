656d062bdbbba6152cd4f9a253006abc8b79ef30
MrSloth-dev <joao8barbosa@gmail.com>
2024-11-26 11:44:18 +0000

    update parser

--- src/00_init/init.c
+++ src/00_init/init.c
@@ -5,11 +5,12 @@ void	ft_init_canvas(t_canvas *canvas)
 	(void)canvas;
 	canvas->count.ambient = 0;
 	canvas->count.camera = 0;
+	canvas->count.light = 0;
 
 	//descomentei abaixo
 	canvas->ambient = ft_init_ambient(0, (t_color){0, 0, 0});
 	canvas->camera = ft_init_camera((t_tuple){0, 0, 0, 0}, (t_tuple){0, 0, 0, 0}, 0);
-	canvas->light = ft_init_light((t_tuple){0, 0, 0, 0}, 0, (t_color){0, 0, 0});
+	canvas->light = ft_init_light((t_tuple){0, 0, 0, 0}, 0.0f, (t_color){0, 0, 0});
 	canvas->sphere = ft_init_sphere((t_tuple){0, 0, 0, 0}, 0, (t_color){0, 0, 0});
 	canvas->plane = ft_init_plane((t_tuple){0, 0, 0, 0},(t_tuple){0, 0, 0, 0}, (t_color){0, 0, 0});
 	canvas->cylinder = ft_init_cylinder((t_tuple){0, 0, 0, 0},(t_tuple){0, 0, 0, 0}, (t_cyl_size){0, 0}, (t_color){0, 0, 0});
--- src/02_parse/03_create_objects.c
+++ src/02_parse/03_create_objects.c
@@ -1,21 +1,59 @@
 #include "minirt.h"
 
+int	ft_vector_check(t_tuple tuple)
+{
+	if (tuple.x == -43.42)
+		return (0);
+	if (tuple.y == -43.42)
+		return (0);
+	if (tuple.z == -43.42)
+		return (0);
+	if (tuple.w == -43.42)
+		return (0);
+	return (1);
+}
+
+int	ft_rgb_check(t_color color)
+{
+	if (color.r < 0 || color.r > 255)
+		return (0);
+	if (color.g < 0 || color.g > 255)
+		return (0);
+	if (color.b < 0 || color.b > 255)
+		return (0);
+	return (1);
+}
+
+int	ft_norm_check(t_tuple norm)
+{
+	if (norm.x < -1 || norm.x > 1)
+		return (0);
+	if (norm.y < -1 || norm.y > 1)
+		return (0);
+	if (norm.z < -1 || norm.z > 1)
+		return (0);
+	if (norm.w < -1 || norm.w > 1)
+		return (0);
+	return (1);
+}
+
 int	ft_create_ambient(t_canvas *canvas, char **split)
 {
 	char	**color_split;
 	int		i;
+	t_color color;
 
 	i = canvas->count.ambient;
 	canvas->ambient[i].ratio = ft_atod(split[1]);
 	color_split = ft_split(split[2], ',');
+	color = (t_color){ft_atod(color_split[0]), ft_atod(color_split[1]),ft_atod(color_split[2])};
 	if (ft_check_null_split(color_split)
-		&& (ft_atod(color_split[1]) != -43.42
-			&& ft_atod(color_split[1]) != -43.42
-			&& ft_atod(color_split[2]) != -43.42))
+		&& ft_rgb_check(color))
 	{
-		canvas->ambient[i].color.r = ft_atod(color_split[0]);
-		canvas->ambient[i].color.g = ft_atod(color_split[1]);
-		canvas->ambient[i].color.b = ft_atod(color_split[2]);
+		canvas->ambient[i].color = color;
+		// canvas->ambient[i].color.r = ft_atod(color_split[0]);
+		// canvas->ambient[i].color.g = ft_atod(color_split[1]);
+		// canvas->ambient[i].color.b = ft_atod(color_split[2]);
 	}
 	else
 		return (ft_free_split(color_split),
@@ -27,35 +65,29 @@ int	ft_create_ambient(t_canvas *canvas, char **split)
 int	ft_create_camera(t_canvas *canvas, char **split)
 {
 	char	**coord_split;
+	t_tuple	coord;
 	char	**norm_split;
+	t_tuple	norm;
 	int		i;
 
 	i = canvas->count.camera;
 	coord_split = ft_split(split[1], ',');
+	coord = (t_tuple){ft_atod(coord_split[0]), ft_atod(coord_split[1]),ft_atod(coord_split[2]), 1};
 	if (ft_check_null_split(coord_split)
-		&& (ft_atod(coord_split[1]) != -43.42
-			&& ft_atod(coord_split[1]) != -43.42
-			&& ft_atod(coord_split[2]) != -43.42))
+		&& ft_vector_check(coord))
 	{
-		canvas->camera[i].coord.x = ft_atod(coord_split[0]);
-		canvas->camera[i].coord.y = ft_atod(coord_split[1]);
-		canvas->camera[i].coord.z = ft_atod(coord_split[2]);
-		// canvas->camera[i].coord.w = 1;
+		canvas->camera[i].coord = coord;
 		ft_free_split(coord_split);
 	}
 	else
 		return (ft_free_split(coord_split),
 			ft_printf(2, "Error, camera format is wrong\n"), 0);
 	norm_split = ft_split(split[2], ',');
+	norm = (t_tuple){ft_atod(norm_split[0]), ft_atod(norm_split[1]),ft_atod(norm_split[2]), 0};
 	if (ft_check_null_split(norm_split)
-		&& (ft_atod(norm_split[1]) != -43.42
-			&& ft_atod(norm_split[1]) != -43.42
-			&& ft_atod(norm_split[2]) != -43.42))
+		&& ft_norm_check(norm))
 	{
-		canvas->camera[i].norm.x = ft_atod(norm_split[0]);
-		canvas->camera[i].norm.y = ft_atod(norm_split[1]);
-		canvas->camera[i].norm.z = ft_atod(norm_split[2]);
-		canvas->camera[i].norm.w = 0;
+		canvas->camera[i].norm = norm;
 		ft_free_split(norm_split);
 	}
 	else
@@ -68,10 +100,38 @@ int	ft_create_camera(t_canvas *canvas, char **split)
 
 int	ft_create_light(t_canvas *canvas, char **split)
 {
-	ft_printf(2, RED"_light\n"RESET);
-	if (ft_count_members(split) != 4)
-		return (ft_printf(2, "Error, light format is wrong\n"), 0);
+	char	**coord_split;
+	t_tuple	coord;
+	char	**color_split;
+	t_color	color;
+	int		i;
+
+	i = canvas->count.light;
+	coord_split = ft_split(split[1], ',');
+	coord = (t_tuple){ft_atod(coord_split[0]), ft_atod(coord_split[1]),ft_atod(coord_split[2]), 1};
+	if (ft_check_null_split(coord_split)
+		&& ft_vector_check(coord))
+	{
+		canvas->light[i].coord = coord;
+		ft_free_split(coord_split);
+	}
+	else
+		return (ft_free_split(coord_split),
+			ft_printf(2, "Error, light format is wrong\n"), 0);
+	color_split = ft_split(split[2], ',');
+	color = (t_color){ft_atod(color_split[0]), ft_atod(color_split[1]),ft_atod(color_split[2])};
+	if (ft_check_null_split(color_split)
+		&& ft_rgb_check(color))
+	{
+		canvas->light[i].color = color;
+		ft_free_split(color_split);
+	}
+	else
+		return (ft_free_split(color_split),
+			ft_printf(2, "Error, light format is wrong\n"), 0);
+	canvas->light[i].bright = ft_atoi(split[1]);
 	canvas->count.light++;
+	printf("coord = %f, %f, %f bright = %f, color r = %f, g = %f, b = %f", canvas->light[i].coord.x,canvas->light[i].coord.y,canvas->light[i].coord.y,canvas->light[i].bright,canvas->light[i].color.r,canvas->light[i].color.g,canvas->light[i].color.b);
 	return (0);
 }
 
