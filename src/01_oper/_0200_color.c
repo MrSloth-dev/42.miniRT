0feae51311955458af1843b14ef8be90e2f650d6
Joao Pedro Oliveira Moreira Albuquerque Barbosa <joao-pol@c2r6s3.42porto.com>
2024-11-27 14:04:15 +0000

    update t_color to typedef t_tuple

--- src/01_oper/_0200_color.c
+++ src/01_oper/_0200_color.c
@@ -2,17 +2,17 @@
 
 int	ft_color_rgb_to_int(t_color color)
 {
-	return ((int)(color.r * 255.0f) << 16
-	| (int)(color.g * 255.0f) << 8
-	| (int)(color.g * 255.0f));
+	return ((int)(color.x * 255.0f) << 16
+	| (int)(color.y * 255.0f) << 8
+	| (int)(color.z * 255.0f));
 }
 
 t_color	ft_color_int_to_rgb(int color)
 {
 	t_color	col;
 
-	col.r = (float)(color >> 16) / 255.0f;
-	col.r = (float)(color >> 8) / 255.0f;
-	col.r = (float)color / 255.0f;
+	col.x = (float)(color >> 16) / 255.0f;
+	col.y = (float)(color >> 8) / 255.0f;
+	col.z = (float)color / 255.0f;
 	return (col);
 }
