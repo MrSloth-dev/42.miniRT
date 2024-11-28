cdc021cc9815803b5b84e3285d09b7baa79db6f3
isilva-t <ivanrfteixeira@gmail.com>
2024-11-28 07:45:05 +0000

    update operator

--- src/01_oper/00_operator.c
+++ src/01_oper/00_operator.c
@@ -2,22 +2,20 @@
 
 t_tuple	ft_operator(t_tuple a, char op, t_tuple b)
 {
-	double	w;
 	t_tuple	r;
 
-	r = (t_tuple){0, 0, 0, 0};
-	w = a.w || b .w;
+	r = (t_tuple){0, 0, 0, a.w || b.w};
 	if (a.w == 3)
-		w = 3;
+		r.w = 3;
 	if (op == '+')
-		r = (t_tuple){a.x + b.x, a.y + b.y, a.z + b.z, w};
+		r = (t_tuple){a.x + b.x, a.y + b.y, a.z + b.z, r.w};
 	else if (op == '-')
 	{
 		if (a.w && b.w && a.w != 3)
-			w = 0;
-		r = (t_tuple){a.x - b.x, a.y - b.y, a.z - b.z, w};
+			r.w = 0;
+		r = (t_tuple){a.x - b.x, a.y - b.y, a.z - b.z, r.w};
 	}
 	else if (op == '*')
-		r = (t_tuple){a.x * b.x, a.y * b.y, a.z * b.z, w};
+		r = (t_tuple){a.x * b.x, a.y * b.y, a.z * b.z, r.w};
 	return (r);
 }
