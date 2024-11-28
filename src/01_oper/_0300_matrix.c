9fdc906c12628fccbce6c4a2237b81cd7baf7ea9
Joao Pedro Oliveira Moreira Albuquerque Barbosa <joao-pol@c2r5s5.42porto.com>
2024-11-28 15:01:27 +0000

    corrected create matrix

--- src/01_oper/_0300_matrix.c
+++ src/01_oper/_0300_matrix.c
@@ -1,26 +1,27 @@
 #include "minirt.h"
+#include <math.h>
 
 t_matrix	ft_create_matrix(int rows, int cols)
 {
 	t_matrix	m;
-	int			i;
-	int			j;
+	int			r;
+	int			c;
 
 	m.rows = rows;
 	m.cols = cols;
-	i = 0;
-	while (i < rows)
+	r = 0;
+	while (r < rows)
 	{
-		j = 0;
-		while (j < cols)
+		c = 0;
+		while (c < cols)
 		{
-			if (j == i)
-				m.data[j][i] = 1;
+			if (c == r)
+				m.data[r][c] = 1;
 			else
-				m.data[j][i] = 0;
-			j++;
+				m.data[r][c] = 0;
+			c++;
 		}
-		i++;
+		r++;
 	}
 	return (m);
 }
@@ -72,7 +73,7 @@ t_matrix	ft_matrix_mult(t_matrix A, t_matrix B)
 	ft_assert(A.cols == B.rows, "diff array size");
 	res = ft_create_matrix(A.cols, B.rows);
 	i = 0;
-	while (i < A.rows)
+	while (i < A.cols)
 	{
 		j = 0;
 		while (j < B.rows)
@@ -90,19 +91,20 @@ t_matrix	ft_matrix_mult(t_matrix A, t_matrix B)
 	}
 	return (res);
 }
-/*
+
 t_tuple	ft_mult_matrix_tuple(t_matrix A, t_tuple B)
 {
 	t_tuple	result;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	ft_tuple_init(&result, (t_point){0, 0, 0}, B.w);
-	result.x = A.data[0][0];
+	result.x = A.data[0][0] * B.x + A.data[0][1] * B.y + A.data[0][2]
+		* B.z + A.data[0][3] * B.w;
+	result.y = A.data[1][0] * B.x + A.data[1][1] * B.y + A.data[1][2]
+		* B.z + A.data[1][3] * B.w;
+	result.z = A.data[2][0] * B.x + A.data[2][1] * B.y + A.data[2][2]
+		* B.z + A.data[2][3] * B.w;
+	result.w = A.data[3][0] * B.x + A.data[3][1] * B.y + A.data[3][2]
+		* B.z + A.data[3][3] * B.w;
 	return (result);
-} */
+}
 
 int	ft_are_matrixes_equal(t_matrix a, t_matrix b)
 {
