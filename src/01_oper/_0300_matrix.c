68918731977affe67a2bd8ea9c7adf1b8cd852e8
joao-pol <joao-pol@student.42porto.com>
2024-11-29 10:09:51 +0000

    update norm matrix

--- src/01_oper/_0300_matrix.c
+++ src/01_oper/_0300_matrix.c
@@ -95,21 +95,23 @@ t_matrix	ft_matrix_mult(t_matrix A, t_matrix B)
 t_tuple	ft_mult_matrix_tuple(t_matrix A, t_tuple B)
 {
 	t_tuple	result;
+
 	result.x = A.data[0][0] * B.x + A.data[0][1] * B.y + A.data[0][2]
-				* B.z + A.data[0][3] * B.w;
+		* B.z + A.data[0][3] * B.w;
 	result.y = A.data[1][0] * B.x + A.data[1][1] * B.y + A.data[1][2]
-				* B.z + A.data[1][3] * B.w;
+		* B.z + A.data[1][3] * B.w;
 	result.z = A.data[2][0] * B.x + A.data[2][1] * B.y + A.data[2][2]
-				* B.z + A.data[2][3] * B.w;
+		* B.z + A.data[2][3] * B.w;
 	result.w = A.data[3][0] * B.x + A.data[3][1] * B.y + A.data[3][2]
-				* B.z + A.data[3][3] * B.w;
+		* B.z + A.data[3][3] * B.w;
 	return (result);
 }
 
 int	ft_are_matrixes_equal(t_matrix a, t_matrix b)
 {
 	int	r;
-	int c;
+	int	c;
+
 	if (a.rows != b.rows || a.cols != b.cols)
 		return (0);
 	r = -1;
@@ -133,7 +135,7 @@ t_matrix	ft_transpose_matrix(t_matrix src)
 	while (++r < src.rows)
 	{
 		c = r;
-		while(++c < src.cols)
+		while (++c < src.cols)
 		{
 			if (c != r)
 			{
