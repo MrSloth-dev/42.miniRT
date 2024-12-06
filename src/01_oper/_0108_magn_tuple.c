757d7742bf0d15536b36bd8d344b7fd41bc1b9aa
isilva-t <ivanrfteixeira@gmail.com>
2024-12-06 21:04:29 +0000

    some norm in 01_oper

--- src/01_oper/_0108_magn_tuple.c
+++ src/01_oper/_0108_magn_tuple.c
@@ -2,6 +2,9 @@
 
 double	ft_magn_tuple(t_tuple tuple) //p7
 {
-	return (sqrt(tuple.x * tuple.x + tuple.y * tuple.y + tuple.z * tuple.z
-			 + tuple.w * tuple.w));
+	return (sqrt(
+			tuple.x * tuple.x
+			+ tuple.y * tuple.y
+			+ tuple.z * tuple.z
+			+ tuple.w * tuple.w));
 }
