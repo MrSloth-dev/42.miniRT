154570b9051fac975f540ffb4639aaf5044cceaa
isilva-t <ivanrfteixeira@gmail.com>
2024-12-18 20:11:02 +0000

    coordinates parser vs world makes me bugged

--- src/04_world/_07_101_create_world_camera.c
+++ src/04_world/_07_101_create_world_camera.c
@@ -37,3 +37,27 @@ t_camera	ft_create_world_camera(double h_size, double v_size, double field_v)
 	cam.px_size = (cam.half_width * 2) / cam.h_size;
 	return (cam);
 }
+
+void	ft_create_world_camera_test(double h_size, double v_size, t_canvas *canvas)
+{
+	double		aspect;
+
+	canvas->camera.h_size = h_size;
+	canvas->camera.v_size = v_size;
+	canvas->camera.field_v = canvas->camera.fov; //COPYED DATA OF SAME STRUCT HERE
+	canvas->camera.transf = ft_create_matrix(4, 4, 1);
+	canvas->camera.inverted = ft_invert_matrix(canvas->camera.transf);
+	canvas->camera.half_view = tan(canvas->camera.field_v / 2);
+	aspect = canvas->camera.h_size / canvas->camera.v_size;
+	if (aspect >= 1)
+	{
+		canvas->camera.half_width = canvas->camera.half_view;
+		canvas->camera.half_height = canvas->camera.half_view / aspect;
+	}
+	else
+	{
+		canvas->camera.half_width = canvas->camera.half_view * aspect;
+		canvas->camera.half_height = canvas->camera.half_view;
+	}
+	canvas->camera.px_size = (canvas->camera.half_width * 2) / canvas->camera.h_size;
+}
