b7c77aa3e96e2f5776288b34d13d9372043586a3
isilva-t <ivanrfteixeira@gmail.com>
2024-12-19 18:06:43 +0000

    crying here. Needs to correct specular

--- src/01_oper/c04_matrix_transform/_0447_rotation.c
+++ src/01_oper/c04_matrix_transform/_0447_rotation.c
@@ -16,12 +16,12 @@ t_tuple	ft_rotation_x(t_tuple tuple, double rad)
 t_tuple	ft_rotation_y(t_tuple tuple, double rad)
 {
 	t_matrix	m;
-//na coordenada [2][0] acho que o sin esta errado, tem que ser negativo
+
 	m = ft_create_matrix(4, 4, 0);
 	ft_set_matrix_values(&m, (double [4][4])
 	{{cos(rad), 0, sin(rad), 0},
 	{0, 1, 0, 0},
-	{sin(rad), 0, cos(rad), 0},
+	{-sin(rad), 0, cos(rad), 0},
 	{0, 0, 0, 1}});
 	return (ft_mult_matrix_tuple(m, tuple));
 }
