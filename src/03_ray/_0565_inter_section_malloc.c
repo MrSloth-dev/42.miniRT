e82ee76483dcfb9bf59ecfdd7f65107a72c7f432
isilva-t <ivanrfteixeira@gmail.com>
2024-12-20 11:01:50 +0000

    update

--- src/03_ray/_0565_inter_section_malloc.c
+++ src/03_ray/_0565_inter_section_malloc.c
@@ -15,19 +15,21 @@ t_inter	*ft_create_inter(double value, t_shapes *shape)
 
 void	ft_lstadd_sort_inter(t_interlst **lst, double value, t_shapes *shape)
 {
-	t_interlst	*temp;
-	t_interlst	*new;
 	t_inter		*node;
-
 	node = ft_create_inter(value, shape);
+
+	t_interlst	*new;
 	new = ft_lstnew(node);
 	if (!lst)
 		lst = &new;
+
+	t_interlst	*temp;
 	if (!*lst)
 		*lst = new;
 	else
 	{
 		temp = *lst;
+
 		if (((t_inter *)temp->cont)->value > value)
 		{
 			new->next = temp;
@@ -37,18 +39,9 @@ void	ft_lstadd_sort_inter(t_interlst **lst, double value, t_shapes *shape)
 		{
 			while (temp->next && (((t_inter *)temp->next->cont)->value < value))
 				temp = temp->next;
+
 			new->next = temp->next;
 			temp->next = new;
 		}
 	}
-}
-
-void	ft_inter_to_list(t_interlst **lst, t_inter *inter)
-{
-	if (!lst)
-		return ;
-	if (!*lst)
-		*lst = ft_lstnew(inter);
-	else
-		ft_lstadd_back(lst, ft_lstnew(inter));
 }
