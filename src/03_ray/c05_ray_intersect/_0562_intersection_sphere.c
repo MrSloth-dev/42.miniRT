6e6620de4bbb5d29627d8d1c400215a3951e1f18
joao-pol <joao-pol@student.42porto.com>
2024-12-20 17:13:55 +0000

    Fixed(kind of) specular fucking finally

--- src/03_ray/c05_ray_intersect/_0562_intersection_sphere.c
+++ src/03_ray/c05_ray_intersect/_0562_intersection_sphere.c
@@ -6,8 +6,7 @@ void	ft_intersection_sphere(t_interlst **lst, t_ray ray, t_shapes *shap)
 	double	discriminant;
 	t_tuple	sphere_to_ray;
 	
-	// sphere_to_ray = ft_sub_tuple(ray.pos, (t_tuple){0, 0, 0, 1});
-	sphere_to_ray = ft_sub_tuple(ray.pos, shap->sph.coord);
+	sphere_to_ray = ft_sub_tuple(ray.pos, (t_tuple){0, 0, 0, 1});
 	h.a = ft_dotprod_vector(ray.dir, ray.dir);
 	h.b = 2 * ft_dotprod_vector(ray.dir, sphere_to_ray);
 	h.d = ft_dotprod_vector(sphere_to_ray, sphere_to_ray) - 1;
