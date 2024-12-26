894e4d43e3be24b79be99016657cc3e5a36941d1
isilva-t <ivanrfteixeira@gmail.com>
2024-12-26 19:54:19 +0000

    cylinders done, need to check light specular in all

--- src/03_ray/c06_light_shading/_09112_normal_at.c
+++ src/03_ray/c06_light_shading/_09112_normal_at.c
@@ -12,14 +12,23 @@ t_tuple	ft_normal_at_plane(t_shapes *shape)
 	return (ft_norm_vector(w_normal));
 }
 
-t_tuple	ft_normal_at_cyl(t_shapes *shape, t_tuple w_point)
+// dist var is used for norm vector when point it's in a cap of cylinder
+t_tuple	ft_normal_at_cyl(t_shapes *shape, t_tuple world_point)
 {
 	t_tuple	object_point;
 	t_tuple	object_normal;
 	t_tuple	world_normal;
+	double	dist;
 
-	object_point = ft_mult_matrix_tuple(shape->inverted, w_point);
-	object_normal = (t_tuple){object_point.x, 0, object_point.z, 0};
+	object_point = ft_mult_matrix_tuple(shape->inverted, world_point);
+
+	dist = object_point.x * object_point.x + object_point.z * object_point.z;
+	if (dist < 1 && object_point.y >= (shape->cyl.size.max - ROUND_ERROR))
+		object_normal = (t_tuple){0, 1, 0, 0};
+	else if (dist < 1 && object_point.y <= (shape->cyl.size.min + ROUND_ERROR))
+		object_normal = (t_tuple){0, -1, 0, 0};
+	else
+		object_normal = (t_tuple){object_point.x, 0, object_point.z, 0};
 	world_normal = ft_mult_matrix_tuple(ft_transpose_matrix(shape->inverted),
 			object_normal);
 	world_normal.w = 0;
