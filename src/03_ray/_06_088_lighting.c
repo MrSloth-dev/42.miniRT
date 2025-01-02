9f24fadd820466b23e78b49ad437313b0b774f61
isilva-t <ivanrfteixeira@gmail.com>
2025-01-02 19:38:02 +0000

    minor update

--- src/03_ray/_06_088_lighting.c
+++ src/03_ray/_06_088_lighting.c
@@ -31,15 +31,15 @@ static t_color	ft_get_final_specular(double reflect_dot_eye, t_canvas *canvas,
 
 t_color	ft_lighting_shadow(t_canvas *canvas, t_comp comp)
 {
-	t_tuple	lightv;
+	t_tuple	light_vector;
 	double	light_dot_normal;
 	double	reflect_dot_eye;
 
 	ft_set_ambient_and_efective_color(canvas, &comp);
 	if (comp.in_shadow == true)
 		return (comp.ambient_on_object);
-	lightv = ft_norm_vector(ft_sub_tuple(canvas->light.coord, comp.over_point));
-	light_dot_normal = ft_dotprod_vector(lightv, comp.normalv);
+	light_vector = ft_norm_vector(ft_sub_tuple(canvas->light.coord, comp.over_point));
+	light_dot_normal = ft_dotprod_vector(light_vector, comp.normalv);
 	reflect_dot_eye = 0;
 	if (light_dot_normal < 0)
 		ft_set_diffuse_and_specular_to_black(&comp);
@@ -48,7 +48,7 @@ t_color	ft_lighting_shadow(t_canvas *canvas, t_comp comp)
 		comp.diffuse = ft_scalar_tuple(ft_scalar_tuple(comp.efective_color,
 					comp.shape->material.diffuse), light_dot_normal);
 		reflect_dot_eye = ft_dotprod_vector(
-				ft_reflect(ft_neg_tuple(lightv), comp.normalv),
+				ft_reflect(ft_neg_tuple(light_vector), comp.normalv),
 				ft_neg_tuple(canvas->camera.norm));
 	}
 	comp.specular = ft_get_final_specular(reflect_dot_eye, canvas, &comp);
