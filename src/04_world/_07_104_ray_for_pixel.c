e139aca3573d2714e65083c57a4705360bf266c7
isilva-t <ivanrfteixeira@gmail.com>
2025-01-02 19:35:42 +0000

    ray.pos on render only do once | better perform intersection list | change var name on lighting

--- src/04_world/_07_104_ray_for_pixel.c
+++ src/04_world/_07_104_ray_for_pixel.c
@@ -12,18 +12,15 @@
 
 #include "minirt.h"
 
-t_ray	ft_ray_for_pixel(t_camera cam, double px, double py)
+void	ft_ray_for_pixel(t_camera cam, double px, double py, t_ray *ray)
 {
 	double		world_x;
 	double		world_y;
 	t_tuple		pixel;
-	t_ray		ret_ray;
 
 	world_x = cam.half_width - ((px + 0.5) * cam.px_size);
 	world_y = cam.half_height - ((py + 0.5) * cam.px_size);
 	pixel = ft_mult_matrix_tuple(cam.inverted,
 			(t_tuple){world_x, world_y, -1, 1});
-	ret_ray.pos = ft_mult_matrix_tuple(cam.inverted, (t_tuple){0, 0, 0, 1});
-	ret_ray.dir = ft_norm_vector(ft_operator(pixel, '-', ret_ray.pos));
-	return (ret_ray);
+	ray->dir = ft_norm_vector(ft_operator(pixel, '-', ray->pos));
 }
