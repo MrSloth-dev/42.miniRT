a738d52292d29121999ff145f79d20deff723d18
isilva-t <ivanrfteixeira@gmail.com>
2025-01-02 16:41:18 +0000

    adjust get_transform_object for cyl | addeed scene folder | update readme

--- src/71_math_utils/_01_008_magn_tuple.c
+++ src/71_math_utils/_01_008_magn_tuple.c
@@ -7,12 +7,3 @@ double	ft_magn_tuple(t_tuple tuple) //p7
 			+ tuple.y * tuple.y
 			+ tuple.z * tuple.z));
 }
-
-/*double	ft_magn_tuple(t_tuple tuple) //p7*/
-/*{*/
-/*	return (sqrt(*/
-/*			tuple.x * tuple.x*/
-/*			+ tuple.y * tuple.y*/
-/*			+ tuple.z * tuple.z*/
-/*			+ tuple.w * tuple.w));*/
-/*}*/
