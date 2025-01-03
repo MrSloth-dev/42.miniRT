abca760658384b04532358b1d8aa6eafcf24fd9d
isilva-t <ivanrfteixeira@gmail.com>
2025-01-03 10:00:21 +0000

    adjust resolution with events

--- src/00_init/init.c
+++ src/00_init/init.c
@@ -1,3 +1,4 @@
+#include "libft.h"
 #include "minirt.h"
 
 void	ft_init_canvas(t_canvas *canvas)
@@ -7,6 +8,8 @@ void	ft_init_canvas(t_canvas *canvas)
 	canvas->count.camera = 0;
 	canvas->count.light = 0;
 	canvas->help = 0;
+	canvas->step = STEP;
+	ft_bzero(canvas->char_step, 2);
 
 	canvas->objects = NULL;
 	canvas->light = ft_init_light((t_tuple){0, 0, 0, 0}, 0.0f, (t_color){0, 0, 0, 0});
