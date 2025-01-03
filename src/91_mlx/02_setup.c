674a022064e346df1a3568a393a41b6e160c145f
joao-pol <joao-pol@student.42porto.com>
2025-01-03 13:58:34 +0000

    reestructuring the main file

--- src/91_mlx/02_setup.c
+++ src/91_mlx/02_setup.c
@@ -2,17 +2,17 @@
 
 static void	ft_events_init(t_canvas *canvas)
 {
-	// mlx_hook(canvas->win, KeyPress, KeyPressMask, key_handler, canvas);
-	// mlx_hook(canvas->win, ButtonPress, ButtonPressMask, mouse_handler, canvas);
-	// mlx_hook(canvas->win, DestroyNotify, StructureNotifyMask, close_handler,
-	// 	canvas);
+	mlx_hook(canvas->win, KeyPress, KeyPressMask, key_handler, canvas);
+	mlx_hook(canvas->win, ButtonPress, ButtonPressMask, mouse_handler, canvas);
+	mlx_hook(canvas->win, DestroyNotify, StructureNotifyMask, close_handler, canvas)
+		;
 	(void)canvas;
 }
 
-int	ft_setup(t_canvas *canvas)
+int	ft_setup(t_canvas *canvas, char *argv[])
 {
+	canvas->name = argv[0] + 2;
 	ft_mlx_init(canvas);
-	// ft_data_init(canvas);
 	ft_events_init(canvas);
 	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -2,45 +2,29 @@
 #include "minirt.h"
 #include <unistd.h>
 
+
+void	ft_mlx_end(t_canvas *canvas)
+{
+	mlx_do_key_autorepeatoff(canvas->mlx);
+	mlx_hook(canvas->win, KeyPress, KeyPressMask, key_handler, canvas);
+	mlx_hook(canvas->win, ButtonPress, ButtonPressMask, mouse_handler, canvas);
+	mlx_hook(canvas->win, DestroyNotify, StructureNotifyMask, close_handler, canvas);
+	mlx_hook(canvas->win, DestroyNotify, 0l, &close_handler, &canvas);
+	mlx_loop(canvas->mlx);
+	ft_free_canvas(canvas);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_canvas	canvas;
 
-	canvas.name = argv[0] + 2;
-	(void)argc;
 	if (argc != 2)
-		return (ft_printf(2, "Error in argv!\n"));
+		return (ft_printf(2, "Error in argv!\n"), 1);
 	ft_init_canvas(&canvas);
-	// ft_mlx_init(&canvas);
-	// ft_setup(&canvas);
 	if (ft_parse(&canvas, argv[1]) == 0)
 		return (1);
-	t_objects *current = canvas.objects;
-	while (current) {
-		if (current->cont) {
-			t_shapes *shape = (t_shapes *)current->cont;
-			switch (shape->type) {
-				case PLANE: printf(YELLOW"PLANE\n"RESET); break;
-				case CYLINDER: printf(YELLOW"CYLINDER\n"RESET); break;
-				case SPHERE: printf(YELLOW"SPHERE\n"RESET); break;
-			}
-		} else {
-			fprintf(stderr, "Warning: current->cont is null\n");
-		}
-		current = current->next;
-	}
-	ft_free_canvas(&canvas);
-	// ft_print_objects(canvas);
-	//
-	// m = ft_create_matrix(4, 4);
-	// ft_set_matrix_values(&m,(double[4][4]){{1,2,3,4}, {5.5,6.5,7.5,8.5}, {9,10,11,12}, {13.5,14.5,15.5,16.5}});
-	// ft_print_matrix(m);
-	// operations_testing();
-	//
-	//jumpingball(&canvas);
-	// ft_draw_square(&canvas, (t_tuple){150, 150, 0, 0}, (t_tuple){150, 150, 0, 0}, 0xFFFF0000);
-	// mlx_key_hook(canvas.win, key_handler, &canvas);
-	// mlx_hook(canvas.win, DestroyNotify, 0L, &close_handler, &canvas);
-	// mlx_loop(canvas.mlx);
+	ft_setup(&canvas, argv);
+	ft_refreshframe(&canvas);
+	ft_mlx_end(&canvas);
 	return (0);
 }
