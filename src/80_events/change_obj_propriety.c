bf377d9193a7fd352418ceb00be1eacf791e338c
isilva-t <ivanrfteixeira@gmail.com>
2025-01-04 23:02:26 +0000

    norminette events files

--- src/80_events/change_obj_propriety.c
+++ src/80_events/change_obj_propriety.c
@@ -7,13 +7,13 @@ static bool	ft_change_obj_dimension(int keysym, t_canvas *canvas)
 	scl = (double)canvas->shift_press / 10;
 	if (keysym == XK_1)
 		ft_get_transf_obj(canvas->object_selected, (t_tuple){0}, (t_tuple){0},
-				 (t_tuple){1 + scl, 1 + scl , 1 + scl, 0});
+			(t_tuple){1 + scl, 1 + scl, 1 + scl, 0});
 	else if (keysym == XK_2 && canvas->object_selected->type == CYLINDER)
 		ft_get_transf_obj(canvas->object_selected, (t_tuple){0}, (t_tuple){0},
-				 (t_tuple){1, 1 + scl * 2 , 1, 0});
+			(t_tuple){1, 1 + scl * 2, 1, 0});
 	else if (keysym == XK_3 && canvas->object_selected->type == CYLINDER)
 		ft_get_transf_obj(canvas->object_selected, (t_tuple){0}, (t_tuple){0},
-				 (t_tuple){1 + scl * 2, 1, 1 + scl * 2, 0});
+			(t_tuple){1 + scl * 2, 1, 1 + scl * 2, 0});
 	else
 		return (false);
 	return (true);
@@ -22,23 +22,23 @@ static bool	ft_change_obj_dimension(int keysym, t_canvas *canvas)
 static bool	ft_change_obj_position(int keysym, t_canvas *canvas)
 {
 	if (keysym == XK_w)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0, 0.3, 0, 0}, (t_tuple){0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0, 0.3, 0, 0}, (t_tuple){0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_s)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0, -0.3, 0, 0}, (t_tuple){0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0, -0.3, 0, 0}, (t_tuple){0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_a)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){-0.3, 0, 0, 0}, (t_tuple){0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){-0.3, 0, 0, 0}, (t_tuple){0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_d)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0.3, 0, 0, 0}, (t_tuple){0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0.3, 0, 0, 0}, (t_tuple){0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_q)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0, 0, 0.3, 0}, (t_tuple){0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0, 0, 0.3, 0}, (t_tuple){0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_e)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0, 0, -0.3, 0}, (t_tuple){0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0, 0, -0.3, 0}, (t_tuple){0}, (t_tuple){1, 1, 1, 0});
 	else
 		return (false);
 	return (true);
@@ -47,17 +47,17 @@ static bool	ft_change_obj_position(int keysym, t_canvas *canvas)
 static bool	ft_change_obj_rotation(int keysym, t_canvas *canvas)
 {
 	if (keysym == XK_Left)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0}, (t_tuple){-0.3, 0, 0, 0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0}, (t_tuple){-0.3, 0, 0, 0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_Right)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0}, (t_tuple){0.3, 0, 0, 0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0}, (t_tuple){0.3, 0, 0, 0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_Up)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0}, (t_tuple){0, 0, 0.3, 0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0}, (t_tuple){0, 0, 0.3, 0}, (t_tuple){1, 1, 1, 0});
 	else if (keysym == XK_Down)
-		ft_get_transf_obj(canvas->object_selected, (t_tuple){0}, (t_tuple){0, 0, -0.3, 0},
-				 (t_tuple){1, 1, 1, 0});
+		ft_get_transf_obj(canvas->object_selected,
+			(t_tuple){0}, (t_tuple){0, 0, -0.3, 0}, (t_tuple){1, 1, 1, 0});
 	else
 		return (false);
 	return (true);
@@ -73,11 +73,11 @@ bool	ft_change_obj_propriety(int keysym, t_canvas *canvas, bool state)
 			;
 		else if (ft_change_obj_position(keysym, canvas))
 			;
-		else if (canvas->object_selected->type != SPHERE 
-				&& ft_change_obj_rotation(keysym, canvas))
+		else if (canvas->object_selected->type != SPHERE
+			&& ft_change_obj_rotation(keysym, canvas))
 			;
 		else if (ft_change_object_color(keysym, canvas))
-				;
+			;
 		else
 		{
 			mlx_do_key_autorepeatoff(canvas->mlx);
--- src/80_events/transform_camera_key.c
+++ src/80_events/transform_camera_key.c
@@ -1,28 +1,46 @@
 #include "minirt.h"
 
+static bool	ft_camera_wasd_keys(t_matrix *m, int key)
+{
+	if (key == XK_a)
+		*m = ft_matrix_mult(*m, ft_translation_matrix(1, 0, 0));
+	else if (key == XK_d)
+		*m = ft_matrix_mult(*m, ft_translation_matrix(-1, 0, 0));
+	else if (key == XK_w)
+		*m = ft_matrix_mult(*m, ft_translation_matrix(0, 0, -1));
+	else if (key == XK_s)
+		*m = ft_matrix_mult(*m, ft_translation_matrix(0, 0, 1));
+	else
+		return (false);
+	return (true);
+}
+
+static bool	ft_camera_arrow_keys(t_matrix *m, int key)
+{
+	if (key == XK_Right)
+		*m = ft_matrix_mult(*m, ft_rotate_matrix_y(M_PI / 20));
+	else if (key == XK_Left)
+		*m = ft_matrix_mult(*m, ft_rotate_matrix_y(-M_PI / 20));
+	else if (key == XK_Up)
+		*m = ft_matrix_mult(*m, ft_rotate_matrix_x(-M_PI / 20));
+	else if (key == XK_Down)
+		*m = ft_matrix_mult(*m, ft_rotate_matrix_x(M_PI / 20));
+	else
+		return (false);
+	return (true);
+}
+
 t_matrix	ft_transform_camera_key(t_matrix m, int key)
 {
 	ft_matrix_mult(m, ft_scale_matrix(1.1, 1, 1.1));
-	if (key == XK_a)
-		return (ft_matrix_mult(m, ft_translation_matrix(1, 0, 0)));
-	else if (key == XK_d)
-		return (ft_matrix_mult(m, ft_translation_matrix(-1, 0, 0)));
-	else if (key == XK_w)
-		return (ft_matrix_mult(m, ft_translation_matrix(0, 0, -1)));
-	else if (key == XK_s)
-		return (ft_matrix_mult(m, ft_translation_matrix(0, 0, 1)));
+	if (ft_camera_wasd_keys(&m, key))
+		;
+	else if (ft_camera_arrow_keys(&m, key))
+		;
 	else if (key == XK_space)
 		return (ft_matrix_mult(m, ft_translation_matrix(0, -1, 0)));
 	else if (key == XK_c)
 		return (ft_matrix_mult(m, ft_translation_matrix(0, 1, 0)));
-	else if (key == XK_Right)
-		return (ft_matrix_mult(m, ft_rotate_matrix_y(M_PI / 20)));
-	else if (key == XK_Left)
-		return (ft_matrix_mult(m, ft_rotate_matrix_y(-M_PI / 20)));
-	else if (key == XK_Up)
-		return (ft_matrix_mult(m, ft_rotate_matrix_x(-M_PI / 20)));
-	else if (key == XK_Down)
-		return (ft_matrix_mult(m, ft_rotate_matrix_x(M_PI / 20)));
 	else if (key == XK_q)
 		return (ft_matrix_mult(m, ft_rotate_matrix_z(-M_PI / 20)));
 	else if (key == XK_e)
