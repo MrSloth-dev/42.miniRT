2b66ff44c92c892528095cc88760ad96aa77e4ef
isilva-t <ivanrfteixeira@gmail.com>
2025-01-04 23:02:26 +0000

    norminette events files

--- src/80_events/change_object_color.c
+++ src/80_events/change_object_color.c
@@ -1,9 +1,8 @@
 #include "minirt.h"
 
-
 static bool	ft_can_change(double actual, double scl)
 {
-	double result;
+	double	result;
 
 	result = actual + scl;
 	if (result > 1 || result < 0)
@@ -18,12 +17,12 @@ static void	ft_change_color_value(t_canvas *canvas, char color)
 	scl = (double)canvas->shift_press / 10;
 	if (color == 'r'
 		&& ft_can_change(canvas->object_selected->material.color.x, scl))
-			canvas->object_selected->material.color.x += scl;
+		canvas->object_selected->material.color.x += scl;
 	else if (color == 'g'
-			&& ft_can_change(canvas->object_selected->material.color.y, scl))
-			canvas->object_selected->material.color.y += scl;
+		&& ft_can_change(canvas->object_selected->material.color.y, scl))
+		canvas->object_selected->material.color.y += scl;
 	else if (color == 'b'
-			&& ft_can_change(canvas->object_selected->material.color.z, scl))
+		&& ft_can_change(canvas->object_selected->material.color.z, scl))
 		canvas->object_selected->material.color.z += scl;
 	else if (color == 't')
 	{
@@ -36,7 +35,7 @@ static void	ft_change_color_value(t_canvas *canvas, char color)
 bool	ft_change_object_color(int keysym, t_canvas *canvas)
 {
 	if (!canvas->object_selected)
-		return (false);	
+		return (false);
 	if (keysym == XK_t)
 		ft_change_color_value(canvas, 't');
 	else if (keysym == XK_r)
