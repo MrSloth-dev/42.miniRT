4e1d967f0eea8ec57182de49bd347beee2a3f537
MrSloth-dev <joao8barbosa@gmail.com>
2025-01-04 21:07:53 +0000

    motherfucker parser

--- parser_testing/parser_testing.sh
+++ parser_testing/parser_testing.sh
@@ -10,26 +10,21 @@ CYAN="\e[1;36m"
 set -uo pipefail
 trap 'rm -f ./gmon.out' EXIT
 
-bin="/home/joao-pol/CommonCore/4.miniRT/miniRT"
 script_dir=$(dirname "$(realpath $0)")
 invalid_dir="${script_dir}"/invalid
-## These tests should fail
-test_files=("test" \
-	"teste.rt" \
-	".rt" \
-	"./teste/invalid_ambient.rt" \
-	"./teste/multiple_ambient.rt" \
-	"./teste/invalid_camera.rt" \
-	"./teste/multiple_camera.rt" \
-	"./teste/invalid_light.rt" \
-	"./teste/multiple_light.rt" \
-	"./input/ex.rt" \
-)
 
+bin="/home/mrsloth/CommonCore/4.miniRT/miniRT"
+if [[ ! -e "${bin}" ]]; then
+	echo "${bin}"
+	echo "No binary found"
+	exit 1
+fi
+
+## These tests should fail
 total=0
 passed=0
 failed=0
-for file in "$invalid_dir"/*; do
+for file in $(find "$invalid_dir" -type f); do
 		((total++))
 		echo -e "Test ${total} :'${file}'"
 		std_err=$(${bin} "${file}" 2>&1)
