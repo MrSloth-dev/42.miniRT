aec742a8770044a904a5def9e389df180029f596
isilva-t <ivanrfteixeira@gmail.com>
2025-01-04 22:04:29 +0000

    update menu instructions

--- src/91_mlx/21_menu.c
+++ src/91_mlx/21_menu.c
@@ -6,33 +6,36 @@ void	ft_menu(t_canvas *canvas)
 
 	if (canvas->help)
 	{
-		i = 15;
+		i = 14;
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - 16 - HEIGHT_POS * i--, WHITE,
 				 "Press 'h' to close help");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - 6 - HEIGHT_POS * i--, WHITE,
 				 "Keyboard commands:");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "\"Esc\" -> exit");
+				 "\"Esc\"       --> exit");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "o p     -> adjust resolution:");
+				 "o p         --> adjust pixel upscale");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "w a s d q e -> camera move");
+				 "w a s d q e --> move");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "Space/c -> camera move up and down");
+				 "Arrow keys  --> rotate");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "r       --> reset camera position");
+				 "Space/c     --> camera move up / down");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "mouse left click -> select object"); //change brightness in object, to sum in lighting function
+				 "z           --> reset camera position");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "x       -> abort selection"); //reset color vars sum
+				 "mouse left click --> select object"); //change brightness in object, to sum in lighting function
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "h j k l -> object moove");
+				 "l           --> select light");
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "9 0     -> adjust scale");
+				 "x           --> abort selection"); //reset color vars sum
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "8 9     -> adjust cylinder height"); 
+				 "Left Shift hold --> subtract enable");	
 		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "r g b   -> adjust red-green-blue color"); 
+				 "r g b      --> adjust R-G-B color"); 
+		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+				 "1 2 3      --> adjust dimensions");
+
 	}
 	else
 	{
