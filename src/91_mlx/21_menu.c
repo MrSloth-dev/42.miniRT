bad8c8e64d41bcc0d07ebedf89032b5004eff797
isilva-t <ivanrfteixeira@gmail.com>
2025-01-05 20:26:10 +0000

    norm menu

--- src/91_mlx/21_menu.c
+++ src/91_mlx/21_menu.c
@@ -1,70 +1,108 @@
 #include "minirt.h"
 
+static void	ft_top_menu_options(t_canvas *canvas, int *j, int i)
+{
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE, "Press 'h' to close help");
+	i--;
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - 6 - HEIGHT_POS * i--, WHITE, "Keyboard commands:");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE, "\"Esc\"       --> exit");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"o p         --> adjust pixel upscale");
+	*j = --i;
+}
+
+static void	ft_general_options(t_canvas *canvas, int *j, int i)
+{
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"w a s d q e --> move");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"Arrow keys  --> rotate");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"Space/c     --> camera move up / down");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"z           --> reset camera position");
+	i--;
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"mouse left click --> select object");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"l           --> select light");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"x           --> abort selection");
+	*j = --i;
+}
+
+static void	ft_last_options(t_canvas *canvas, int *j, int i)
+{
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"Left Shift hold --> subtract enable");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"r g b      --> adjust R-G-B color");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"t    --> adjust bright object/light");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"1 2 3      --> adjust dimensions");
+	i--;
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"Left shift + 0        --> SURPRISE!");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"(only with pixel upscale minimum 2)");
+	*j = --i;
+}
+
+static void	ft_small_menu_info(t_canvas *canvas, int i)
+{
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
+		"Press 'h' for help");
+	mlx_string_put(canvas->mlx, canvas->win,
+		W_POS, WIN_H - HEIGHT_POS * i, WHITE,
+		"Pixel upscale:");
+	if (canvas->step > 9)
+	{
+		canvas->char_step[0] = '1';
+		canvas->char_step[1] = canvas->step - 10 + '0';
+	}
+	else
+	{
+		canvas->char_step[0] = '0';
+		canvas->char_step[1] = canvas->step + '0';
+	}
+	mlx_string_put(canvas->mlx, canvas->win, W_POS + 105,
+		WIN_H - HEIGHT_POS * i--, WHITE,
+		canvas->char_step);
+}
+
 void	ft_menu(t_canvas *canvas)
 {
 	int	i;
 
 	if (canvas->help)
 	{
-		i = 19;
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "Press 'h' to close help");
-		i--;
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - 6 - HEIGHT_POS * i--, WHITE,
-				 "Keyboard commands:");
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "\"Esc\"       --> exit");
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "o p         --> adjust pixel upscale");
-		i--;
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "w a s d q e --> move");
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "Arrow keys  --> rotate");
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "Space/c     --> camera move up / down");
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "z           --> reset camera position");
-		i--;
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "mouse left click --> select object"); //change brightness in object, to sum in lighting function
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "l           --> select light");
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "x           --> abort selection"); //reset color vars sum
-		i--;
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "Left Shift hold --> subtract enable");	
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "r g b      --> adjust R-G-B color"); 
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "t    --> adjust bright object/light"); 
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "1 2 3      --> adjust dimensions");
-
+		i = 22;
+		ft_top_menu_options(canvas, &i, i);
+		ft_general_options(canvas, &i, i);
+		ft_last_options(canvas, &i, i);
 	}
 	else
 	{
 		i = 2;
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i--, WHITE,
-				 "Press 'h' for help");
-		mlx_string_put(canvas->mlx, canvas->win, W_POS, WIN_H - HEIGHT_POS * i, WHITE,
-				 "Pixel upscale:");
-		if (canvas->step > 9)
-		{
-			canvas->char_step[0] = '1';
-			canvas->char_step[1] = canvas->step - 10 + '0';
-		}
-		else
-		{
-			canvas->char_step[0] = '0';
-			canvas->char_step[1] = canvas->step + '0';
-		}
-		mlx_string_put(canvas->mlx, canvas->win, W_POS + 105 , WIN_H - HEIGHT_POS * i--, WHITE,
-				 canvas->char_step);
-		
-
-
+		ft_small_menu_info(canvas, i);
 	}
-		
 }
