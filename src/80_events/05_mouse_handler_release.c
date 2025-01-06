d54593d4a0821933bbff337d743b72c2e4ff4581
isilva-t <ivanrfteixeira@gmail.com>
2025-01-06 10:23:19 +0000

    add some headers

--- src/80_events/05_mouse_handler_release.c
+++ src/80_events/05_mouse_handler_release.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   05_mouse_handler_release.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isilva-t <isilva-t@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 09:59:12 by isilva-t          #+#    #+#             */
+/*   Updated: 2025/01/06 09:59:13 by isilva-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
 int	mouse_handler_release(int mousecode, int x, int y, t_canvas *canvas)
