b6f7a4c33e8a6f04a445a9663e0fefcf1946e511
isilva-t <ivanrfteixeira@gmail.com>
2025-01-07 13:37:14 +0000

    better rotation user experience

--- src/80_events/04_mouse_handler.c
+++ src/80_events/04_mouse_handler.c
@@ -14,13 +14,27 @@
 
 int	mouse_handler(int mousecode, int x, int y, t_canvas *canvas)
 {
+	int		camera_invert;
+
+	camera_invert = 1;
 	if (mousecode == 3)
 		canvas->mouse_drag = 1;
 	if (mousecode == 1)
 		ft_select_obj(x, y, canvas);
-	else if (mousecode == 4)
-		canvas->camera.field_v -= M_PI / 30;
+	if (mousecode == 4 || mousecode == 5)
+		canvas->camera.transf = ft_matrix_mult(canvas->camera.transf, canvas->camera.inv_rotate);
+	if (mousecode == 4)
+	{
+		canvas->camera.transf = ft_matrix_mult(canvas->camera.transf, ft_translation_matrix(0, 0, -1));
+	}
 	else if (mousecode == 5)
-		canvas->camera.field_v += M_PI / 30;
+		canvas->camera.transf = ft_matrix_mult(canvas->camera.transf, ft_translation_matrix(0, 0, 1));
+	else
+		camera_invert = 0;
+	if (camera_invert == 1)
+	{
+		canvas->camera.transf = ft_matrix_mult(canvas->camera.transf, canvas->camera.rotate);
+		canvas->camera.inverted = ft_invert_matrix(canvas->camera.transf);
+	}
 	return (0);
 }
