5ce58f1e48999f8e967f6b010ba011acd1e947b7
joao-pol <joao-pol@student.42porto.com>
2025-01-08 16:25:17 +0000

    update Makefile I FUCKING LOVE BASH

--- src/makefile.sh
+++ src/makefile.sh
@@ -2,10 +2,16 @@
 
 # names of directories
 for dir in $(find . -mindepth 1 -type d); do
-	echo -n ${dir^^} | sed 's/.\///';
+	echo -n "${dir^^}DIR" | sed 's/.\///';
 	echo " = ${dir}" | sed 's/\./src/'
+	echo -n "${dir^^} = " | sed 's/.\///';
 	for file in $(find "${dir}" -type f -name '*.c' -exec basename {} \;); do
-		echo -e "\t\$(${dir^^})/$file \ " | sed 's/(\.\//(/'
+		echo -e "\t\$(${dir^^}DIR)/$file \ " | sed 's/(\.\//(/'
 	done
 	echo ""
+done
+
+echo -ne "SRCS = "
+for dir in $(find . -mindepth 1 -type d); do
+	echo -ne "\$(${dir^^}) " | sed 's/(\.\//(/'
 done
